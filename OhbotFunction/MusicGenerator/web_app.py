e696552cd9aa289f6e731d629ca015dbc307a0d5
Ztadeas <74866388+Ztadeas@users.noreply.github.com>
2022-11-03 09:54:31 +0100

    Add files via upload

--- OhbotFunction/MusicGenerator/web_app.py
+++ OhbotFunction/MusicGenerator/web_app.py
@@ -1,10 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 
-app = Flask("__main__")
+app = Flask("__main__", static_folder="C:\\own programs\\music_generator\\static")
 
-@app.route("/")
+genres = []
+genresGen = {
+  "cllasical": {
+    "default": True,
+    "title": "Classical music",
+  },
+  "folk": {
+    "default": True,
+    "title": "Folk",
+  },
+  "rap": {
+    "default": False,
+    "title": "Rap",
+  },
+  "rock": {
+    "default": False,
+    "title": "Rock",
+  },
+  "soundtrack": {
+    "default": False,
+    "title": "Soundtrack",
+  },
+  "country": {
+    "default": False,
+    "title": "Country",
+  },
+}
+
+for k, v in genresGen.items():
+  genres.append(k)
+start_dg = random.randint(1, 101)
+
+@app.route("/", methods=["POST", "GET"])
 def home():
-  return render_template("home.html")
+  #start_dg = random.randint(1, 101)
+  to_use = ["classical", "folk", "country"]
+  checkBoxes = {}
+  for i in genres:
+    checkBoxes[i] =""
+  if request.method == "POST":
+    dg =  request.form["dgf"]
+    new_to_use = []
+    for i in genres:
+      try:
+        request.form[i]
+        checkBoxes[i] = "checked"
+        new_to_use.append(i)
+      
+      except:
+        pass
+
+    to_use = new_to_use
+    start_dg = dg
+  
+  else:
+    start_dg = random.randint(1, 101)
+    
+
+   
+
+  return render_template("home.html", start_dgf=start_dg, checkBoxes=checkBoxes, genresGen=genresGen)
 
 
 if __name__ == "__main__":
