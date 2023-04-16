8a9281507f22e44c44aff64eb792a7ed95812728
Viktor Cihal <viktor.cihal@gmail.com>
2023-04-16 22:24:46 +0200

    Add eleven labs speech

--- Function/Speech/talking.py
+++ Function/Speech/talking.py
@@ -1,12 +1,63 @@
-import pyttsx3
+import requests
+import os
+from pydub import AudioSegment
+from pydub.playback import play
 
 
 class TalkingController:
-    def __init__(self):
-        self.engine = pyttsx3.init()
-        self.engine.setProperty('rate', 150)
-        self.engine.setProperty('volume', 0.9)
+    """
+    Talking is currently working via eleven labs API. Therefore, there is a network connection required.
+    """
 
-    def say(self, text):
-        self.engine.say(text)
-        self.engine.runAndWait()
+    API_KEY = "aade8d9ee7f3c2bc0e8fbf286c2c78c0"
+    VOICE_OPTIONS = {
+        "Rachel": "21m00Tcm4TlvDq8ikWAM",
+        "Domi": "AZnzlk1XvdvUeBnXmlld",
+        "Bella": "EXAVITQu4vr4xnSDxMaL",
+        "Antoni": "ErXwobaYiN019PkySvjV",
+        "Elli": "MF3mGyEYCl7XYWbV9V6O",
+        "Josh": "TxGEqnHWrfWFTfGW9XjX",
+        "Arnold": "VR6AewLTigWG4xSOukaG",
+        "Adam": "pNInz6obpgDQGcFmaJgB",
+        "Sam": "yoZ06aMxZJJ28mfd3POQ",
+    }
+    VOICE_ID = VOICE_OPTIONS["Adam"]
+    SPEECH_FILE = "tts.wav"
+
+    def __init__(self):
+        self._headers = {
+            "Content-Type": "application/json",
+            "xi-api-key": self.API_KEY,
+        }
+        tts_url = (
+            f"https://api.elevenlabs.io/v1/voices"
+        )
+        response = requests.get(tts_url, headers=self._headers)
+
+    def say(self, text: str) -> bool:
+        """Speak text using elevenlabs.io's API
+        Args:
+            text (str): The text to speak
+            voice_index (int, optional): The voice to use. Defaults to 0.
+        Returns:
+            bool: True if the request was successful, False otherwise
+        """
+        tts_url = (
+            f"https://api.elevenlabs.io/v1/text-to-speech/{self.VOICE_ID}"
+        )
+        response = requests.post(tts_url, headers=self._headers, json={"text": text})
+
+        if response.status_code == 200:
+            audio_data = response.content
+            with open(self.SPEECH_FILE, "wb") as f:
+                f.write(audio_data)
+            speech = AudioSegment.from_mp3(self.SPEECH_FILE)
+            play(speech)
+            os.remove(self.SPEECH_FILE)
+            return True
+
+        else:
+            print("Request failed with status code:", response.status_code)
+            print("Response content:", response.content)
+            return False
+
