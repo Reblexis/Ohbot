4a853a7af87d5f1f48b342c6ff0ca20da2bf7410
Viktor Cihal <viktor.cihal@gmail.com>
2023-08-27 21:27:55 +0200

    Improve command parsing and finish basic brain

--- Function/Behaviour/brain.py
+++ Function/Behaviour/brain.py
@@ -23,28 +23,30 @@ class BrainController(ABC):
 class GPT3BrainController(BrainController):
     API_FILE_PATH = OTHER_FOLDER / "openai_api_key.txt"
 
-    BEHAVIOUR_PROMPT = "You are controlling a robot. Available commands are: `say`, `set`, `help`, `pass`." \
+    BEHAVIOUR_PROMPT = "You are controlling a robot. Available commands are: `say`, `toggle`, `help`, `pass`." \
                        "If you want to use a command, you should first learn more about it by typing `help [command]`. After this you get response" \
                        "from the system describing the command and its usage. You can then determine if you want to use it or find one that suits you better." \
                        "If you think that doing nothing is the best response, respond with pass and nothing will happen." \
                        "You can only respond with commands and nothing else. Your answer can be as short as you want." \
                        "This is VERY IMPORTANT you can only respond with the commands."
 
-    messages = [
-        {"role": "system", "content": BEHAVIOUR_PROMPT},
-        {"role": "user", "content": "Hello how are you?"},
-        {"role": "assistant", "content": "help say"},
-        {"role": "system",
-         "content": "Command: say\nDescription: Makes the robot say something.\nParameters: text (The text to say.)\nExample: say 'Hello!'"},
-        {"role": "assistant", "content": "say 'Hello. I'm doing fine thank you. How are you?'"},
-    ]
-
     def __init__(self, command_manager: CommandManager):
         super().__init__(command_manager)
         openai.api_key = open(self.API_FILE_PATH, "r").read()
+        self.messages = []
+        self.initialize_messages()
+
+    def initialize_messages(self):
+        self.messages = [
+            {"role": "system", "content": self.BEHAVIOUR_PROMPT},
+            {"role": "user", "content": "Hello how are you?"},
+            {"role": "assistant", "content": "help say"},
+            {"role": "system",
+             "content": command_manager.help({"command": "say"})},
+            {"role": "assistant", "content": "say --text=\"Hello. I'm doing fine thank you\""},
+        ]
 
     def get_response(self, recursion_counter: int = 0) -> dict[str, str] | dict[str, str | Any]:
-        print(self.messages)
         if recursion_counter > 3:
             return {"response": "This is a scripted message. I don't know what to do."}
 
@@ -56,6 +58,8 @@ class GPT3BrainController(BrainController):
         self.messages.append({"role": "assistant", "content": response_content})
         command = response_content.split(" ")[0]
 
+        print(self.messages)
+
         if command == "pass":
             return {"response": "pass"}
 
@@ -65,7 +69,10 @@ class GPT3BrainController(BrainController):
             self.messages.append({"role": "system", "content": system_response})
             return {"response": self.get_response(recursion_counter + 1)}
 
-        self.command_manager.execute_command(response_content)
+        is_correct, status_message = self.command_manager.execute_command(response_content)
+        if not is_correct:
+            self.messages.append({"role": "system", "content": status_message})
+            return {"response": self.get_response(recursion_counter + 1)}
 
         return {"response": response}
 
